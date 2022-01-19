fa4f31f61cf134aae104d910132bbea787dda0b4
Phoenix616 <max@themoep.de>
2022-01-19 14:30:29 +0100

    Add alias support (v1.1)

--- src/main/java/de/themoep/whatplugin/WhatPlugin.java
+++ src/main/java/de/themoep/whatplugin/WhatPlugin.java
@@ -7,6 +7,7 @@ import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Arrays;
+import java.util.Locale;
 
 /**
  * WhatPlugin
@@ -29,15 +30,31 @@ public class WhatPlugin extends JavaPlugin {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (args.length > 0) {
             String commandStr = args[0].startsWith("/") ? args[0].substring(1) : args[0];
+
+            // Try to get alias
+            String[] aliased = getServer().getCommandAliases().get(commandStr);
+            if (aliased == null) {
+                aliased = getServer().getCommandAliases().get(commandStr.toLowerCase(Locale.ROOT));
+            }
+            if (aliased != null) {
+                sender.sendMessage(ChatColor.GREEN + "Found alias " + ChatColor.YELLOW + commandStr + ChatColor.GREEN + ":");
+                for (String alias : aliased) {
+                    sender.sendMessage(ChatColor.GREEN + "- " + ChatColor.YELLOW + alias);
+                }
+                return true;
+            }
+
+            // Try to get plugin command
             PluginCommand command = getServer().getPluginCommand(commandStr);
             if (command != null) {
                 sender.sendMessage(ChatColor.GREEN + "Found command " + ChatColor.YELLOW + command.getName() + ChatColor.GREEN + " in plugin " + ChatColor.YELLOW + command.getPlugin().getName());
                 sender.sendMessage(ChatColor.GREEN + "Description: " + ChatColor.YELLOW + (command.getDescription() != null ? command.getDescription() : "None? What does it do???"));
                 sender.sendMessage(ChatColor.GREEN + "Permission: " + ChatColor.YELLOW + (command.getPermission() != null ? command.getPermission() : "None? That's strange!"));
                 sender.sendMessage(ChatColor.GREEN + "Aliases: " + ChatColor.YELLOW + (command.getAliases().size() > 0 ? (Arrays.toString(command.getAliases().toArray())) : "None"));
-            } else {
-                sender.sendMessage(ChatColor.RED + "No plugin command with the name " + ChatColor.YELLOW + commandStr + ChatColor.RED + " found? Maybe it is a Vanilla command?");
+                return true;
             }
+
+            sender.sendMessage(ChatColor.RED + "No plugin command with the name " + ChatColor.YELLOW + commandStr + ChatColor.RED + " found? Maybe it is a Vanilla command?");
             return true;
         }
         return false;
