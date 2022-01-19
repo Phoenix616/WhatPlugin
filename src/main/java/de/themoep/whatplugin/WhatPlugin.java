5298656504c539b5e77b3a0beb7f7e0dab458827
Phoenix616 <max@themoep.de>
2022-01-19 14:56:28 +0100

    Use class loader too to detect plugin
    Also add backwards compatibility to spigot with reflections

--- src/main/java/de/themoep/whatplugin/WhatPlugin.java
+++ src/main/java/de/themoep/whatplugin/WhatPlugin.java
@@ -2,16 +2,20 @@ package de.themoep.whatplugin;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandMap;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.PluginClassLoader;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.Locale;
 
 /**
  * WhatPlugin
- * Copyright (C) 2015 Max Lee (https://github.com/Phoenix616/)
+ * Copyright (C) 2022 Max Lee (https://github.com/Phoenix616/)
  * <p/>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -45,10 +49,11 @@ public class WhatPlugin extends JavaPlugin {
             }
 
             // Try to get plugin command
-            Command command = getServer().getCommandMap().getCommand(commandStr);
+            Command command = getCommandMap().getCommand(commandStr);
             if (command != null) {
+                Plugin plugin = getPlugin(command);
                 sender.sendMessage(ChatColor.GREEN + "Found command " + ChatColor.YELLOW + command.getName() + ChatColor.GREEN + " in "
-                        + (command instanceof PluginCommand ? ("plugin " + ChatColor.YELLOW + ((PluginCommand) command).getPlugin().getName()) : "server"));
+                        + (plugin != null ? ("plugin " + ChatColor.YELLOW + plugin.getName()) : "server"));
                 sender.sendMessage(ChatColor.GREEN + "Description: " + ChatColor.YELLOW + command.getDescription());
                 sender.sendMessage(ChatColor.GREEN + "Permission: " + ChatColor.YELLOW + (command.getPermission() != null ? command.getPermission() : "None? That's strange!"));
                 sender.sendMessage(ChatColor.GREEN + "Aliases: " + ChatColor.YELLOW + (command.getAliases().size() > 0 ? (Arrays.toString(command.getAliases().toArray())) : "None"));
@@ -59,6 +64,32 @@ public class WhatPlugin extends JavaPlugin {
             return true;
         }
         return false;
+    }
+
+    private CommandMap getCommandMap() {
+        try {
+            return getServer().getCommandMap();
+        } catch (NoSuchMethodError e) {
+            try {
+                Field commandMap = getServer().getClass().getField("commandMap");
+                commandMap.setAccessible(true);
+                return (CommandMap) commandMap.get(getServer());
+            } catch (NoSuchFieldException | IllegalAccessException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    private Plugin getPlugin(Command command) {
+        if (command instanceof PluginCommand) {
+            return ((PluginCommand) command).getPlugin();
+        }
+        ClassLoader loader = command.getClass().getClassLoader();
+        if (loader instanceof PluginClassLoader) {
+            return ((PluginClassLoader) loader).getPlugin();
+        }
+        return null;
     }
 
 }
